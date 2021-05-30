80ca0286fc38f36f9b75fa5538f543bdaf842392
Tom Siemion <tsiemion07@gmail.com>
2021-05-30 16:38:07 -0500

    added header and User Table component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import UserTable from './components/UserTable'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid p-0">
+        <h1 className="text-center p-4 bg-secondary text-light">User Directory</h1>
+      </div>
+      <UserTable />
+  
+    </>
   );
 }
 
